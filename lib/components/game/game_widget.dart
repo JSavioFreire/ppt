47ff887754bd812e96c33a6d3de102ce554cba74
Sávio Freire <105501774+JSavioFreire@users.noreply.github.com>
2022-12-12 10:40:18 -0300

    fazendo

--- lib/components/game/game_widget.dart
+++ lib/components/game/game_widget.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
 
 class Game extends StatelessWidget {
-  var resultado;
-  Game(this.resultado, {super.key});
+  final String resultado;
+  final String enemy;
+  const Game(this.resultado, this.enemy, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(resultado),
+    return SizedBox(
+      width: double.infinity,
+      height: 500,
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [Text('Seu oponente:')],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/components/state/state_widget.dart
+++ lib/components/state/state_widget.dart
@@ -12,46 +12,57 @@ class StateWidget extends StatefulWidget {
 }
 
 class _StateWidgetState extends State<StateWidget> {
+  String resultado = '';
+  String enemyPlay = 'enemy';
+
   @override
   Widget build(BuildContext context) {
-    String resultado = 'vazio';
-
     inPlay(props) {
       int enemyChoise = Random().nextInt(3);
 
       if (props == 'pedra') {
         if (enemyChoise == 0) {
           resultado = 'empate';
+          enemyPlay = 'pedra';
         } else if (enemyChoise == 1) {
           resultado = 'derrota';
+          enemyPlay = 'papel';
         } else if (enemyChoise == 2) {
           resultado = 'vitoria';
+          enemyPlay = 'tesoura';
         }
       } else if (props == 'papel') {
         if (enemyChoise == 0) {
-          print('inimigo jogou pedra, vitoria');
+          resultado = 'vitoria';
+          enemyPlay = 'pedra';
         } else if (enemyChoise == 1) {
-          print('inimigo jogou papel, empate');
+          resultado = 'empate';
+          enemyPlay = 'papel';
         } else if (enemyChoise == 2) {
-          print('inimigo jogou tesoura, derrota');
+          resultado = 'derrota';
+          enemyPlay = 'tesoura';
         }
       } else if (props == 'tesoura') {
         if (enemyChoise == 0) {
-          print('inimigo jogou pedra, derrota');
+          resultado = 'derrota';
+          enemyPlay = 'pedra';
         } else if (enemyChoise == 1) {
-          print('inimigo jogou papel, vitoria');
+          resultado = 'vitoria';
+          enemyPlay = 'papel';
         } else if (enemyChoise == 2) {
-          print('inimigo jogou tesoura, empate');
+          resultado = 'empate';
+          enemyPlay = 'tesoura';
         }
       }
+      setState(() {
+        resultado;
+      });
     }
-
- 
 
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        Game(resultado),
+        Game(resultado, enemyPlay),
         Column(
           children: [const Matchs(), Play(inPlay)],
         ),
